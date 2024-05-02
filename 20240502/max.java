2f1a08c942e1703a5e61c3313492170c175bbc8b
MWP <83859867+Minwoo-AlvinPark@users.noreply.github.com>
2024-05-02 15:01:31 +0900

    max 동작 함수로 추출

--- 20240502/max.java
+++ 20240502/max.java
@@ -1,12 +1,17 @@
 class Max {
   public static void main(String[] args) {
     int[] input = {45,3,5,3,2,25,6};
+     
+    System.out.println(findMax(input));
+  }
+
+  public static int findMax(int[] input) {
     int max = input[0];
-    for(int i= 0; i< input.lenght; i++) {
+    for(int i = 0; i < input.lenght; i++) {
       if(max < input[i]) {
         max = input[i];
       }
     }
-    System.out.println(max);
+    return max;
   }
 }
