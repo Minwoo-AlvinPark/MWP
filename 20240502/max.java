7f949929b649ca6edf25245cb61256edde715866
MWP <83859867+Minwoo-AlvinPark@users.noreply.github.com>
2024-05-02 15:00:21 +0900

    Create max.java

---
+++ 20240502/max.java
@@ -0,0 +1,12 @@
+class Max {
+  public static void main(String[] args) {
+    int[] input = {45,3,5,3,2,25,6};
+    int max = input[0];
+    for(int i= 0; i< input.lenght; i++) {
+      if(max < input[i]) {
+        max = input[i];
+      }
+    }
+    System.out.println(max);
+  }
+}
